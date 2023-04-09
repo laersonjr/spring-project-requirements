6cb53f784753a99b5849914c9f83f4315703beb8
laerson <laersonjr@gmail.com>
2023-04-09 12:08:51 -0300

    :tada: Commit inicial

---
+++ project-requirements/src/main/java/com/laerson/projectrequirements/ProjectRequirementsApplication.java
@@ -0,0 +1,13 @@
+package com.laerson.projectrequirements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRequirementsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectRequirementsApplication.class, args);
+    }
+
+}
---
+++ project-requirements/src/test/java/com/laerson/projectrequirements/ProjectRequirementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laerson.projectrequirements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRequirementsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
